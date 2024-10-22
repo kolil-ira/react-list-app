2c82325397ebd833d205166d534334b93f64745d
ira <irakolil31@gmail.com>
2024-10-22 12:18:28 +0300

    change1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
     { id: 2, title: '1984', author: 'George Orwell' },
     { id: 3, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-    { id: 4, title: 'Moby-Dick', author: 'Herman Melville' }
+    { id: 4, title: 'MobDick', author: 'Herman Melville' }
   ];
 
   return (
